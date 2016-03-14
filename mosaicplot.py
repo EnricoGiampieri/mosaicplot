6e000abd9a306168bbabd2343e299040ad01ee42
EnricoGiampieri <enrico.giampieri@unibo.it>
2016-03-14 16:11:15 +0100

    complete teoretical pipeline

--- mosaicplot.py
+++ mosaicplot.py
@@ -10,7 +10,11 @@ import pandas as pd
 import pylab as plt
 import numpy as np
 import itertools as it
+from collections import namedtuple
+from functools import partial
 import operator as op
+import seaborn
+import toolz
 # %%
 """
 how the data is expected:
@@ -70,21 +74,31 @@ genero la tabella dei conteggi
 """
 data_ = data.copy()
 data_['Intercept'] = 1
-info_df = data_.groupby(['Sex', 'Married', 'No_of_Children'])['Intercept'].count()
-print(info_df)
+info_df_ = data_.groupby(['Sex', 'No_of_Children', 'Married'])['Intercept'].count()
+print(info_df_)
+
+# %%
+url = "http://facweb.cs.depaul.edu/mobasher/classes/csc478/Data/titanic-trimmed.csv"
+titanic = pd.read_csv(url)
+titanic.head(10)
+titanic['Intercept'] = 1
+titanic['survived'] = titanic['survived'].replace({0:'No', 1:'Yes'})
+info_df_ = titanic.groupby(['pclass', 'sex', 'survived'])['Intercept'].count()
+
+
 # %%
 """
 faccio in modo che tutte le ctegorie siano piene.
 in teoria i conteggi vanno
 """
+info_df = info_df_.copy()
 p = pd.MultiIndex.from_product(info_df.index.levels, names=info_df.index.names)
 info_df = info_df.reindex(p, fill_value=0)
-
+prior = 0.5*0
+info_df += prior
 levels = info_df.index.names
 labels = list(map(list, info_df.index.levels))
-print(info_df)
 
-print(labels)
 # this check is to ensure the proper behavior later one, where we need this trick
 assert info_df.equals(info_df[tuple()])
 # pick the sublist fromt he list given of elements that share the same
@@ -125,11 +139,19 @@ hierachical_extent = [(*i, sum(j[1] for j in sameRoot(i, hierachical_index)[:i[3
 hierachical_extent_2 = [(i[0], i[4], i[1], i[2], i[3]) for i in hierachical_extent]
 # now the spacing. I scale starting points and lenght based on the number of elements
 # and the size of the scaling
-spacing = 0.05
-hierachical_spacing = [(i[0], i[1]/(1+spacing*(i[3]-1)), i[2]/(1+spacing*(i[3]-1)), i[3], i[4])
+spacing = [0.05, 0.01] + [0]*(len(labels)-2)
+hierachical_spacing = [(i[0],
+                        i[1]/(1+spacing[len(i[0])-1]*(i[3]-1)),
+                        i[2]/(1+spacing[len(i[0])-1]*(i[3]-1)),
+                        i[3],
+                        i[4])
                        for i in hierachical_extent_2]
 # now I have to move the starting point accordingly to put everything back in place
-hierachical_spacing_2 = [(i[0], i[1]+i[4]*spacing/(1+spacing*(i[3]-1)), i[2], i[3], i[4])
+hierachical_spacing_2 = [(i[0],
+                          i[1]+i[4]*spacing[len(i[0])-1]/(1+spacing[len(i[0])-1]*(i[3]-1)),
+                          i[2],
+                          i[3],
+                          i[4])
                          for i in hierachical_spacing]
 # group, x, width, y, height, size, rank, parity of the group (horizontal or vertical)
 hierachical_spacing_3 = [(*i, ((-1)**len(i[0]))==-1) for i in hierachical_spacing_2]
@@ -155,80 +177,61 @@ for i in hierachical_spacing_3:
     final_division.append(new_i)
 final_division = final_division[1:]
 assert len(final_division) == len(hierachical_spacing_3)
-# keep only those that are part of the last subdivision
-subgroups_division = [i for i in final_division if len(i[0]) == len(labels)]
 
+rectStruct = namedtuple('RectStruct', ['code', 'x', 'width', 'y', 'height'])
+final_division_named = [rectStruct(*i) for i in final_division]
 
-subgroups_division_df = pd.DataFrame(subgroups_division)
-subgroups_division_df.set_index(0, inplace=True)
-subgroups_division_df.columns = ['x', 'width', 'y', 'heigth']
-subgroups_division_df.index = pd.MultiIndex.from_tuples(subgroups_division_df.index)
-subgroups_division_df.index.names = info_df.index.names
-subgroups_division_df['counts'] = info_df
+# %%
+current_palette = seaborn.color_palette()*5
+#seaborn.palplot(current_palette)
 
+colori = list(it.chain.from_iterable(seaborn.dark_palette(current_palette[i], len(labels[2])+1)[::-1][:-1]
+                                for i in range(len(labels[1]))))
+#seaborn.palplot(colori)
+etichette = list(it.product(*labels[1:]))
+etichette2color = dict(zip(etichette, colori))
+
+max_len = max(len(k.code) for k in final_division_named)
+with seaborn.axes_style("white"):
+    fig, ax = plt.subplots(1, 1)
+    ax.grid(False)
+    for (k, x, w, y, h) in final_division_named:
+        if len(k)<max_len:
+            continue
+        text = " ".join(str(i) for i in k)
+        r = plt.Rectangle((x, y), w, h,
+                          facecolor=etichette2color[k[1:3]],
+                          edgecolor='gray',
+                          label=text)
+        ax.add_patch(r)
+        if w==0 or h==0:
+            continue
+
+    ax.set_xticks([k.x+k.width/2 for k in final_division_named if len(k.code)==1])
+    ax.set_xticklabels([k.code[0] for k in final_division_named if len(k.code)==1])
+    ax.set_xlabel(levels[0])
+
+    y_ticks = [(k.code[1], k.y+k.height/2) for k in final_division_named if len(k.code)==2]
+    y_ticks = [(k, np.mean([i[1] for i in v])) for k, v in toolz.groupby(lambda i:i[0], y_ticks).items()]
+
+    ax.set_yticks([i[1] for i in y_ticks])
+    ax.set_yticklabels([i[0] for i in y_ticks])
+    ax.set_ylabel(levels[1])
+
+    ax_clone = ax.twiny()
+    ax_clone.grid(False)
+    z_ticks = [((k.code[0], k.code[2]), k.x+k.width/2) for k in final_division_named if len(k.code)==3]
+    z_ticks = [(k, np.mean([i[1] for i in v])) for k, v in toolz.groupby(lambda i: i[0], z_ticks).items()]
+    ax_clone.set_xticks([i[1] for i in z_ticks])
+    ax_clone.set_xticklabels([i[0][1] for i in z_ticks], rotation=90)
+    ax_clone.set_xlabel(levels[2])
+
+    handles = {n:plt.Rectangle((0,0),1,1, color=c) for n, c in etichette2color.items()}
+    obj, clr = zip(*sorted(handles.items()))
+
+    ax.legend(clr,
+              [" - ".join(map(str, i)) for i in obj],
+              bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
 # %%
 
 # %%
-fig, ax = plt.subplots(1, 1)
-for k, (x, w, y, h) in subgroups_division_df.iterrows():
-    r = plt.Rectangle((x, y), w, h, facecolor='lightgray', edgecolor='gray')
-    ax.add_patch(r)
-    if w==0 or h==0:
-        continue
-    ax.text(x+w/2, y+h/2, "\n".join(str(i) for i in k),
-            horizontalalignment='center',
-            verticalalignment='center')
-
-
-# %%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
